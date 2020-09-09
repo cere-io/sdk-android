e8b38de7574871afe43d9ecb1fd3130809aab9f8
Rudolf Markulin <rudolf.markulin@protonmail.com>
2020-09-09 12:14:08 +0200

    Added webView to CereApp fragment

--- cere_sdk/src/main/java/io/cere/cere_sdk/CereApp.kt
+++ cere_sdk/src/main/java/io/cere/cere_sdk/CereApp.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_cere_app.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,6 +42,11 @@ class CereApp : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cere_app, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        webview.loadUrl("http://6527c6acb350.ngrok.io")
     }
 
     // TODO: Rename method, update argument and hook method into UI event
