46cfd912f02a431ee8d88a8aa49d9b2071b9b3c3
Rudolf Markulin <rudolf.markulin@protonmail.com>
2020-09-14 10:18:09 +0200

    Remove unused showToast method

--- cere_sdk/src/main/java/io/cere/cere_sdk/WebAppInterface.kt
+++ cere_sdk/src/main/java/io/cere/cere_sdk/WebAppInterface.kt
@@ -4,17 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.webkit.JavascriptInterface
-import android.widget.Toast
 
 class WebAppInterface(private val mContext: Context) {
 
     private val TAG = this::class.java.simpleName
-
-    /** Show a toast from the web page  */
-    @JavascriptInterface
-    fun showToast(toast: String) {
-        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
-    }
 
     @JavascriptInterface
     fun engagementReceived() {
